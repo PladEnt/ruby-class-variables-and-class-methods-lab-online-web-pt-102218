579ad4e8c92b2cea404a2736cb928cc12c26a1e3
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-11-13 15:33:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,10 @@
 class Song
   attr_accessor :name, :artist, :genre
+  
   @@count = 0
   @@genres = []
   @@artists = []
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
@@ -12,13 +14,18 @@ class Song
     @@genres << genre
     @@artists << artist
   end
+  
   def self.count
     @@count
   end
+  
   def self.genre
     @@genres.uniq
   end
+  
   def self.artist
     @@artists.uniq
   end
+  
+  
 end
