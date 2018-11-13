3164d7d9c166f153636b7fd0bbec3d21856f1ca1
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-11-13 15:13:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,26 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@genre = []
+  @@artist = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genre.each do |tipe|
+      if tipe 
+      @@genre << genre
+    end
+    @@artist << artist
   end
   def count
     puts @@count
+  end
+  def genre
+    puts @@genre
+  end
+  def artist
+    puts @@artist
   end
 end
